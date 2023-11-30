9d88fa6589e458e37a4deb9ca8a29ff19b1c6f65
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-30 23:10:11 +0545

    Learnt about while loop and funcitons

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,39 @@
-This is our too much amazing custom theme.
+<?php 
+?>
+
+<p>Hi, my name is <?php echo $names[0] ?></p>;
+
+<!-- 
+WHILE LOOP:
+$names = array("Beegyan", "Roneet", "Rajeev", "Sapkota");
+    $count = 0;
+
+    while($count < count($names)){
+        echo "<li>Hi , my name is $names[$count]</li>";
+        $count++;
+    }
+
+WHILE LOOP:
+    $names = array("Beegyan", "Roneet", "Rajeev", "Sapkota");
+    $count = 1;
+
+    while($count < 100) {
+        echo "<li>$count</li>";
+        $count++;
+    }
+
+FUNCITON:
+    function myFirstFunction () {
+        echo "<pre>Hello it's me Bigyan.</pre>";
+    }
+    myFirstFunction();
+    myFirstFunction(); 
+
+FUNCTION WITH ARGUMENTS:
+    echo(bloginfo('name'));
+    function greet($name, $color) {
+        echo "<p>Hi, my name is $name & my favorite color is $color</p>";
+    }
+    greet("Bigyan", "Black");
+    greet("Cutie", "Pink");
+-->
